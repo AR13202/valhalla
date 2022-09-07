7d632aeef58f330db7dd362aae6ddc7101b397f8
Mayank-Tayal <tayalmayank60@gmail.com>
2022-09-08 00:36:41 +0530

    Second Change

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import Img from "./components/Logo.jpg";
+import "./index.css";
 function App() {
   
      return<>
      <Navbar />
+      <div className="container">
+        <div className="logo"><img src={Img}  alt="" /></div>
+        <div className="letsStart">HI</div>
+      </div>
      </>
       
     
